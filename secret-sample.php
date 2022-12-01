0821472888b09da4bafbd603677c3664770e563e
Berta Besteiro <berta.besteiro@gmail.com>
2022-12-01 09:09:41 +0100

    Añadido secret-sample

---
+++ secret-sample.php
@@ -0,0 +1,42 @@
+<?php
+//////////////////////////////
+// Renombrar a secret.php una vez puestos los valores adecuados
+//////////////////////////////
+//credenciales de redmine comun
+$userRedmineComun = "";
+$passRedmineComun = "";
+$apiRedmineComun = "";
+$urlRedmineComun = "";
+$urlUploadsComun = "";
+//$projectId = ""; hay varios proyectos
+
+//ids de los usuarios redmine común
+$idCategoryDoceo = "";
+$idCategoryWordPress = "";
+$idCategoryAularagon = "";
+$idCategorySTEAM = "";
+$idCategoryAramoodle = "";
+$idCategoryAeducar = "";
+$idCategoryFP = "";
+$idCategoryCDD = "";
+$idCategoryOtros = "";
+
+//credenciales de redmine vitalinux
+$userRedmineVx = "";
+$passRedmineVx = "";
+$apiRedmineVx = "";
+$urlRedmineVx = "";
+$urlUploadsVx = "";
+
+//ids de los usuarios redmine vitalinux
+$idCategoryVitalinux = "";
+
+//Valores del servidor SMTP
+$SMTPserver = "";
+$SMTPport = ;
+$SMTPsender = "";
+$SMTPsenderName = "";
+$SMTPpassword = "";
+
+
+?>
