315a7b18455fa7b6bc0e3c913d8f0cc7b9f932cc
Berta Besteiro <berta.besteiro@gmail.com>
2022-12-07 11:05:57 +0100

    Página administración modificada

--- admin/redmine-contact.php
+++ admin/redmine-contact.php
@@ -14,8 +14,16 @@ if (!current_user_can('administrator')) {
     <div id="mentorias_cdd" class="wrap">
         <h1 class="wp-heading-inline">Soporte CATEDU</h1>
         <div class="wrap">
-            <p>Al activar este plugin se crea una página llamada Soporte Catedu, que incluye un formulario para enviar incidencias al redmine de soporte de CATEDU</p>
-            <p>Es importante no modificar esta página</p>
+            <p>Al activar este plugin se crea una página llamada <b>Soporte Catedu</b>, que incluye un formulario para enviar incidencias al redmine de soporte de CATEDU</p>
+            <p>ES IMPORTANTE NO ENTRAR A EDITAR ESTA PÁGINA.</p>
+            <p>Si se entra a editar se desconfigura todo el código.</p>
+            <h2 class="title">Qué hacer si se desconfigura la página</h2>
+            <p>Si se desconfigura la página hay que:</p>
+            <ol>
+                <li>Desactivar el plugin</li>
+                <li>Activar el plugin</li>
+                <li>Volver a enlazar la página al menú correspondiente</li>
+            </ol>
         </div>
     </div>
 
